878d27b61ec2b0c858054e2be6a3b4bf8786d895
jacob dengate <jdengate6@gmail.com>
2026-05-31 17:23:24 -0400

    Week 2 Programming assignment

--- Week01_Programming_Assignment/Week01_Programming_Assignment.cpp
+++ Week01_Programming_Assignment/Week01_Programming_Assignment.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    cout << "Welcome to my program which you can use to track your gareden plants!" << endl;
+
     string plant_type; //Type of Plant
+    double plant_price; //Price of Plant
+    int amt_of_seeds; //amt of seeds planted
     int plant_date;   //Date Plant was Planted
     int last_watered; //Date Plant was Last Watered
     int todays_date; //Todays Date
     int days_old;    //How Many Days Old the Plant is
     int days_water;  //How Many Days Since the Plant was Last Watered
 
+    ofstream outdata;
+
     cout << "Enter Plant Type:";
     cin >> plant_type;
 
+    cout << "Enter Price of plant seeds:";
+    cin >> plant_price;
+
+    cout << "Enter how many seeds planted:";
+    cin >> amt_of_seeds;
+    
     cout << "Enter Date Planted in 3 Digit Date Format:";
     cin >> plant_date;
 
@@ -26,7 +40,14 @@ int main()
     days_old = todays_date - plant_date;
     days_water = todays_date - last_watered;
 
-    cout << "Your " << plant_type << " crop is " << days_old << " days old, and was last watered " << days_water << " days ago." << endl;
+    outdata << fixed << showpoint;
+    outdata.open("report.txt");
+
+    outdata << "-------------- " << plant_type << " Summary " << "--------------" << endl ;
+    outdata << setw(10) << "Price" << setw(10) << "Seed #" << setw(10) << "Age" << setw(12) << "Last Water" << endl;
+    outdata << setw(6) << "$" << setprecision(2) << plant_price << setw(8) << amt_of_seeds << setw(10) << days_old << setw(12) << days_water;
+    
+    outdata.close();
 
     
 
